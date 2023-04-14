f653967e548a929b7f96bf238f42ec11606450cf
Yaterie <128695574+Yaterie@users.noreply.github.com>
2023-04-14 07:40:22 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rect(130, 130, 150)
+  rect(240, 60, 35)
+  triangle(205, 30, 100, 130, 310, 130)
+  rect(160, 160, 30)  
+  rect(220, 210, 35)
+  rect(220, 245, 35)
+  
+}
