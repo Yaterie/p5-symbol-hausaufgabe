eba3ebbda9eb6f939a99dcff8b2afa12ee03f384
Yaterie <128695574+Yaterie@users.noreply.github.com>
2023-04-14 07:42:36 +0200

    symbol variables

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+let x = 0;
+let y = 0;
+
 
 function setup() {
   createCanvas(400, 400);
@@ -5,11 +8,11 @@ function setup() {
 
 function draw() {
   background(220);
-  rect(130, 130, 150)
-  rect(240, 60, 35)
-  triangle(205, 30, 100, 130, 310, 130)
-  rect(160, 160, 30)  
-  rect(220, 210, 35)
-  rect(220, 245, 35)
+  rect(130+x, 130+y, 150)
+  rect(240+x, 60+y, 35)
+  triangle(205+x, 30+y, 100+x, 130+y, 310+x, 130+y)
+  rect(160+x, 160+y, 30)  
+  rect(220+x, 210+y, 35)
+  rect(220+x, 245+y, 35)
   
 }
